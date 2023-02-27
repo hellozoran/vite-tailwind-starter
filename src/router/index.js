04fb5a95c7b60b67869ec9aad52052d3080bbb2c
Zoran Mitrovic <32069164+hellozoran@users.noreply.github.com>
2023-02-27 10:51:47 -0600

    feature: Added layout template and meta in router, created jsconfig

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '../views/Home.vue'
+import Blank from '../layout/Blank.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      layout: Blank
+    }
   }
 ]
 
